6991b1b9348b75a1b0ecd73be9388bc897dc69f4
Beyza Senol <beyzasenol08@gmail.com>
2022-10-02 17:03:50 +0300

    hacker rank solution

---
+++ HackerRank/String/Solution.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        
+        char charA=A.charAt(0);
+        int asciiA= (int) charA ;
+        
+        char charB=B.charAt(0);
+        int asciiB=(int) charB;
+        
+        int sum=A.length()+B.length();
+        System.out.println(sum);
+        
+        if ((asciiA<90 && asciiA>65) && (asciiB<90 && asciiB>65)){
+            if(asciiA>asciiB){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+        }
+        if ((asciiA<122 && asciiA>97) && (asciiB<122 && asciiB>97)){
+            if(asciiA>asciiB){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+        }
+        String upperA=A.substring(0,1).toUpperCase()+A.substring(1,A.length());
+        String upperB=B.substring(0,1).toUpperCase()+B.substring(1);
+        
+        System.out.println(upperA+" "+ upperB);
+               
+    }
+}
+
+
+
